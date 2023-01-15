a7f780e9b9543c5c0c155db84798e018c1a282d4
sitb157 <sitb157@gmail.com>
2023-01-15 19:33:20 +0900

    test exit

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sf ${HOME}/dotfiles/dotfiles/vimrc ~/.vimrc
-vim +PluginInstall +'call mkdp#util#install()' +qall
+vim +PluginInstall +qall
+vim +'call mkdp#util#install()' &
+exit
