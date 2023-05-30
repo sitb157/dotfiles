34e87b9c9e04f22676ee8d41318aa603c1a19453
sitb157 <108820413+sitb157@users.noreply.github.com>
2023-05-30 15:40:15 +0900

    Update setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/svermeulen/vundle.git ~/.vim/bundle/vundle
 ln -sf ${HOME}/dotfiles/dotfiles/vimrc ~/.vimrc
+ln -sf ${HOME}/dotfiles/dotfiles/hyper.js ~/.hyper.js
 vim +BundleInstall +qall
 vim +'call mkdp#util#install()' &
 exit
