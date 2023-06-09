41a955af8d12b11d09e6582b96224b8c695245b8
sitb157 <108820413+sitb157@users.noreply.github.com>
2023-06-09 14:46:11 +0900

    Update hyper.js

--- dotfiles/hyper.js
+++ dotfiles/hyper.js
@@ -180,8 +180,8 @@ module.exports = {
   keymaps: {
     // Example
     // 'window:devtools': 'cmd+alt+o',
-    'pane:splitRight': ['ctrl+o'],
-    'pane:splitDown': ['ctrl+e'],
+    'pane:splitRight': ['ctrl+e'],
+    'pane:splitDown': ['ctrl+o'],
     'tab:new': ['alt+t', 'cmd+t'],
     'tab:prev': ['alt+h', 'cmd+h'],
     'tab:next': ['alt+l', 'cmd+l'],
