0cbccd7d84f1add34f6608fb2f3c53a0f4044661
sitb157 <108820413+sitb157@users.noreply.github.com>
2023-06-16 14:29:48 +0900

    Update hyper.js

--- dotfiles/hyper.js
+++ dotfiles/hyper.js
@@ -170,7 +170,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-pane", "hyperpower"],
+  plugins: ["hyper-pane", "hyperpower", "hyperyellow"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
